f3a11a3c666e317f5aa9d540d150d30aeda7c6e6
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2023-05-11 12:57:06 +0000

    apply model to 10 entries from test data

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 import spacy
+import pandas as pd
 
 med7 = spacy.load("en_core_med7_lg")
 
@@ -10,16 +11,22 @@ for label, colour in zip(med7.pipe_labels['ner'], seven_colours):
 
 options = {'ents': med7.pipe_labels['ner'], 'colors':col_dict}
 
+
+def apply_model(text):
+    doc = med7(text)
+    spacy.displacy.render(doc, style='ent', jupyter=True, options=options)
+    return [(ent.text, ent.label_) for ent in doc.ents]
+
 text = 'A patient was prescribed Magnesium hydroxide 400mg/5ml suspension PO of total 30ml bid for the next 5 days.'
-doc = med7(text)
-
-spacy.displacy.render(doc, style='ent', jupyter=True, options=options)
-
-[(ent.text, ent.label_) for ent in doc.ents]
+apply_model(text)
 
 # Loading test data
-#test_data = pd.read_csv("parsed_unit_test_data.csv",
-#                        index_col = 0 )
+test_data = pd.read_csv("parsed_unit_test_data.csv",
+                        index_col = 0 )
 
 # Taking 10 random entries
-#test_data_short = test_data.sample(n=10, random_state=1)
+test_data_short = test_data.sample(n=10)
+
+for t in test_data_short.input:
+    apply_model(t)
+
