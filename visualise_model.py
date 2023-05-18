69f73771c600ae39f789fbb4e34c6dcdba5dfeeb
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2023-05-18 09:49:32 +0000

    get files from ner update example

---
+++ visualise_model.py
@@ -0,0 +1,15 @@
+#https://github.com/explosion/projects/blob/v3/pipelines/ner_demo_update/scripts/visualize_model.py
+import spacy_streamlit
+import typer
+
+
+def main(models: str, default_text: str):
+    models = [name.strip() for name in models.split(",")]
+    spacy_streamlit.visualize(models, default_text, visualizers=["ner"])
+
+
+if __name__ == "__main__":
+    try:
+        typer.run(main)
+    except SystemExit:
+        pass
