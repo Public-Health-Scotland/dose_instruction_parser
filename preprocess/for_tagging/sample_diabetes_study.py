d97b19b028f4c9394b8ee7aaf250f0ee26d202b2
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2023-05-23 09:22:39 +0000

    get test data from diabetes

--- preprocess/for_tagging/sample_diabetes_study.py
+++ preprocess/for_tagging/sample_diabetes_study.py
@@ -1,26 +1,38 @@
 from random import random
 import re
 
-# Sampling 2% from Diabetes study
-lines_edi = [line for line in \
-         open("***REMOVED***2223-0181_edi_cleanRx/data/redacted_sid")\
-         if random() <= .001]
 
-lines_epr = [line for line in \
-         open("***REMOVED***2223-0181_epr_cleanRx/data/redacted_sid")\
-         if random() <= .001]
+def get_diabetes_sample(percent=2):
 
-lines = lines_edi + lines_epr
+    # Divide by 2 as get 1/2 amount from each file
+    sample_num = percent/(2*100)
 
-# Remove the line number tags (everything up to and including |)
-lines = [re.sub(r'^.*?\|', '', line) for line in lines]
-# Strip newlines
-#lines = [line.rstrip() for line in lines]
+    # Sampling from Diabetes study
+    lines_edi = [line for line in \
+            open("***REMOVED***2223-0181_edi_cleanRx/data/redacted_sid")\
+            if random() <= sample_num]
 
-# Write to text file
-outfile = open("preprocess/for_tagging/sample_diabetes_study.txt", "w")
-outfile.writelines(lines)
-outfile.close()
+    lines_epr = [line for line in \
+            open("***REMOVED***2223-0181_epr_cleanRx/data/redacted_sid")\
+            if random() <= sample_num]
+
+    lines = lines_edi + lines_epr
+
+    # Remove the line number tags (everything up to and including |)
+    lines = [re.sub(r'^.*?\|', '', line) for line in lines]
+
+    return lines
+
+# Getting 2% sample for tagging
+diabetes_for_tagging = get_diabetes_sample()
+with open("preprocess/for_tagging/sample_diabetes_study.txt", "w") as outfile:
+    outfile.writelines(diabetes_for_tagging)
+
+# Getting testing 1% sample
+diabetes_for_testing = get_diabetes_sample(1)
+with open("preprocess/for_testing/sample_diabetes_study_test.txt", "w") as outfile:
+    outfile.writelines(diabetes_for_testing)
+
 
 
 
