2b1a0422e7e0a58117a93f8a9b5dae59167a13bb
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2023-06-20 07:49:40 +0000

    adapt preprocessing script for universal data tool

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -1,33 +1,41 @@
+"""
+Loads .udt.json files from preprocess/tagged/ and converts them to spacy
+format for training 
+"""
+from os import listdir
 from spacy.tokens import DocBin
 import spacy
 import json
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 
-files = ['diabetes_first_806.json','diabetes_rest.json','first170.json']
+# Get all .udt.json files in filepath
+filepath = "preprocess/tagged/"
+files = [f for f in listdir(filepath) if f.endswith(".udt.json")]
 
+# Using med7 model as a base 
 nlp = spacy.load("en_core_med7_lg")
 
 def load_data(file):
-    with open(f"preprocess/tagged/{file}", "r", encoding="utf-8") as f:
+    with open(f"{filepath}/{file}", "r", encoding="utf-8") as f:
         data = json.load(f)
-    return (data["annotations"])
+    return (data["samples"])
 
 # Load data into one flat list
 alldata = [load_data(file) for file in files]
 alldata = [item for sublist in alldata for item in sublist if item is not None]
 
-# Shuffle and split into tr/val
+# Shuffle and split into tr/dev
 train_data, dev_data = train_test_split(alldata, test_size=0.33, random_state=6)
 
 # Convert json to spacy format
 def create_training(TRAIN_DATA):
     db = DocBin()
-    for text, annot in tqdm(TRAIN_DATA):
-        doc = nlp.make_doc(text)
+    for el in tqdm(TRAIN_DATA):
+        doc = nlp.make_doc(el["document"])
         ents = []
-        for start, end, label in annot["entities"]:
-            span = doc.char_span(start, end, label=label,
+        for ent in el["annotation"]["entities"]:
+            span = doc.char_span(ent["start"], ent["end"], label=ent["label"],
                                  alignment_mode="contract")
             if span is None:
                 print("Skipping entity")
