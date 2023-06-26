ed9a731363fd0fdb35bd9b528e3f5e96bf062e2d
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2023-06-26 07:49:21 +0000

    use original json format (ner annotator)

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 
 # Get all .udt.json files in filepath
 filepath = "preprocess/tagged/"
-files = [f for f in listdir(filepath) if f.endswith(".udt.json")]
+files = [f for f in listdir(filepath) if f.endswith(".json")]
 
 # Using med7 model as a base 
 nlp = spacy.load("en_core_med7_lg")
@@ -19,7 +19,7 @@ nlp = spacy.load("en_core_med7_lg")
 def load_data(file):
     with open(f"{filepath}/{file}", "r", encoding="utf-8") as f:
         data = json.load(f)
-    return (data["samples"])
+    return (data["annotations"])
 
 # Load data into one flat list
 alldata = [load_data(file) for file in files]
@@ -31,11 +31,11 @@ train_data, dev_data = train_test_split(alldata, test_size=0.33, random_state=6)
 # Convert json to spacy format
 def create_training(TRAIN_DATA):
     db = DocBin()
-    for el in tqdm(TRAIN_DATA):
-        doc = nlp.make_doc(el["document"])
+    for text, annot in tqdm(TRAIN_DATA):
+        doc = nlp.make_doc(text)
         ents = []
-        for ent in el["annotation"]["entities"]:
-            span = doc.char_span(ent["start"], ent["end"], label=ent["label"],
+        for start, end, label in annot["entities"]:
+            span = doc.char_span(start, end, label=label,
                                  alignment_mode="contract")
             if span is None:
                 print("Skipping entity")
