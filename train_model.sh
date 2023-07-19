9670696027d4b55a8c6103222b88ee83d0e2d698
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2023-07-19 12:33:18 +0000

    output to logfile

--- train_model.sh
+++ train_model.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-python -m spacy train ./config/config.cfg --output ./output --paths.train ./data/train.spacy --paths.dev ./data/dev.spacy
+# Naming log file with today's time and date
+today=`date '+%m_%d__%H_%M_%S'`;
+filename="logs/train_$today.log"
+
+# Write out config file to log
+cat ./config/config/cfg | $filename
+cat "\n\n\n" | $filename
+
+# Write out training output to log
+python -m spacy train ./config/config.cfg --output ./output --paths.train ./data/train.spacy --paths.dev ./data/dev.spacy | tee $filename
