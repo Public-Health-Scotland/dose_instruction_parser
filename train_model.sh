dfc1f4fd8cd6cb5c1403e1a4b895b044f688ac8e
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2023-09-01 10:22:30 +0000

    debug pipes

--- train_model.sh
+++ train_model.sh
@@ -5,8 +5,9 @@ today=`date '+%m_%d__%H_%M_%S'`;
 filename="logs/train_$today.log"
 
 # Write out config file to log
-cat ./config/config/cfg | $filename
-cat "\n\n\n" | $filename
+touch $filename
+cat ./config/config.cfg >> $filename
+echo $"\r\n" >> $filename
 
 # Write out training output to log
-python -m spacy train ./config/config.cfg --output ./output --paths.train ./data/train.spacy --paths.dev ./data/dev.spacy | tee $filename
+python -m spacy train ./config/config.cfg --output ./output --paths.train ./data/train.spacy --paths.dev ./data/dev.spacy >> $filename
