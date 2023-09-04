a8b8c01908229f55b1b7f15ca94f5b7f7a7bafeb
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2023-09-04 08:59:34 +0000

    need to get info from test data

--- example/score.py
+++ example/score.py
@@ -12,30 +12,31 @@ med7 = spacy.load("en_core_med7_lg")
 model_best = spacy.load("output/model-best")
 model_last = spacy.load("output/model-last")
 
-# Get all .json files in filepath
-filepath = "preprocess/processed/"
-files = ["crosschecked_data.dat", "resolved_data.dat"]
+# # Get all .json files in filepath
+# filepath = "preprocess/processed/"
+# files = ["crosschecked_data.dat", "resolved_data.dat"]
 
-def process_line(line):
-    selection, text, ann = line.replace("\n", "").split(" | ")
-    ann = ast.literal_eval(ann)
-    return [text, ann]
+# def process_line(line):
+#     selection, text, ann = line.replace("\n", "").split(" | ")
+#     ann = ast.literal_eval(ann)
+#     return [text, ann]
 
-def load_processed_dat(file, filepath=filepath):
-    with open(f"{filepath}/{file}", "r") as f:
-        lines = f.readlines()
-    lines = [process_line(line) for line in lines]
-    return lines
+# def load_processed_dat(file, filepath=filepath):
+#     with open(f"{filepath}/{file}", "r") as f:
+#         lines = f.readlines()
+#     lines = [process_line(line) for line in lines]
+#     return lines
 
-processed_data = [load_processed_dat(file) for file in files]
-processed_data = sorted([item for sublist in processed_data for item in sublist if item is not None], key=lambda x: x[0], reverse=True)
+# processed_data = [load_processed_dat(file) for file in files]
+# processed_data = sorted([item for sublist in processed_data for item in sublist if item is not None], key=lambda x: x[0], reverse=True)
+
+test_file = "data/test.spacy"
 
 def get_scores(ner_model, samples):
     scorer = Scorer(ner_model)
     example = []
     for sample in tqdm(samples):
         pred = ner_model(sample[0])
-        #print(pred, sample[1]['entities'])
         temp_ex = Example.from_dict(pred, {'entities': sample[1]['entities']})
         example.append(temp_ex)
     scores = scorer.score(example)
