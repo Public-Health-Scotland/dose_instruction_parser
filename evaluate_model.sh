2ba3692e2105e5312b87c0ae75d416ef57ad642d
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2023-09-05 09:10:21 +0000

    evaluate model using bash script

---
+++ evaluate_model.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+usage="$(basename "$0") [-h] -- script to evaluate performance of a given model on data/test.spacy
+
+You must provide as an argument the model you want to test.
+This can be by name or location 
+For example:
+./evaluate_model en_core_med7_lg
+./evaluate_model output/model-best
+"
+if [ "$1" == "-h" ]; then
+  echo "Usage: $usage"
+  exit 0
+fi
+
+# Naming log file with today's time and date
+today=`date '+%m_%d__%H_%M_%S'`;
+filename="logs/evaluate_$1_$today.log"
+touch $filename
+echo $1 >> $filename
+
+python -m spacy evaluate $1 data/test.spacy >> $filename
--- train_model.sh
+++ train_model.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+usage="$(basename "$0") [-h] -- script to train model
+
+The model is trained on the data in data/train.spacy and data/dev.spacy
+Output is in output/model-best and output/model-last
+Logs are found in logs with train prefix and current timestamp.
+
+"
+if [ "$1" == "-h" ]; then
+  echo "Usage: $usage"
+  exit 0
+fi
+
 # Naming log file with today's time and date
 today=`date '+%m_%d__%H_%M_%S'`;
 filename="logs/train_$today.log"
