c94b2e0a181e7db198ea25084267930a643bc5a1
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2023-10-24 12:27:01 +0000

    fix bug in checker script

--- di_parser/di_checker.py
+++ di_parser/di_checker.py
@@ -1,7 +1,11 @@
 import csv
 import spacy
+import logging
 import di_parser.di_parser as dip
 
+logging.basicConfig(filename="di_parser/di_check.log", level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger=logging.getLogger(__name__)
 
 checkfile = "***REMOVED***data/for tagging/dose_instructions_3_rp.txt"
 checklines = [line for line in open(checkfile)]
@@ -10,11 +14,15 @@ model_path = "***REMOVED***models/"
 di_parser = dip.DIParser(model_name=f"{model_path}/original/model-best")
 model = spacy.load(f"{model_path}/original/model-best")
 
-with open("di_checking.txt", "w+") as outfile:
+with open("di_parser/di_checking.txt", "w+") as outfile:
     for line in checklines:
-        di = di_parser.parse(line)
-        ents = dip._get_model_entities_from_text(line, model)
-        outfile.write( line + "\n\t" + str(ents) + "\n\t" + str(di) + "\n\n")
+        try:
+            di = di_parser.parse(line)
+            ents = dip._get_model_entities_from_text(line, model)
+            outfile.write( line + "\n\t" + str(ents) + "\n\t" + str(di) + "\n\n")
+        except Exception as e:
+            logger.warning(f"Error in dose instruction: {line}")
+            logger.error(e)
 
 
 
