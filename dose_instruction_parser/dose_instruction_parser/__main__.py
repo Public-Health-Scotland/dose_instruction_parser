0e8fc9f37dc535492805b648bc0576223dd14bf9
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2023-12-20 14:53:38 +0000

    first attempt at main function

--- dose_instruction_parser/dose_instruction_parser/__main__.py
+++ dose_instruction_parser/dose_instruction_parser/__main__.py
@@ -1,15 +1,37 @@
+import os
+import argparse
+from dotenv import load_dotenv
 from dose_instruction_parser import di_parser
 
+# TODO: arguments for outfile, parallelise
+# Separate function for parsing 1 or many dis
 def main():
     """Parse dose instructions"""
     # Arguments given
-    # 0: dose instruction
-    # 1: model path 
-    di = sys.argv[0]
-    model_path = sys.argv[1]
+    # 1: dose instruction
+    # 2: model path 
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-di", "--doseinstruction", required=True)
+    ap.add_argument("-mp", "--modelpath", required=True)
+    args = ap.parse_args()
 
-    di_parser = di_parser.DIParser(model_name=model_path)
-    parsed_di = di_parser.parse(di)
+    dip = di_parser.DIParser(model_name=args.modelpath)
 
-    return parsed_di
+    # Check if valid filepath provided for dis
+    if not os.path.exists(args.doseinstruction):
+        parsed_di = dip.parse(args.doseinstruction)
+        print(parsed_di)
+        return parsed_di
+    else:
+        try:
+            with open(args.doseinstruction, "r") as file:
+                dis = file.readlines()
+            out = [dip.parse(di) for di in dis]
+            for line in out: print(line)
+        except OSError:
+            print(f"Could not open file: {args.doseinstruction}")
+
+
     
+if __name__ == "__main__":
+    main()
