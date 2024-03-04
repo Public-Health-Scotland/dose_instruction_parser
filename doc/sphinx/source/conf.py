1b841c803a60e2c37b5e0ea25ab4063607403054
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2024-03-04 09:58:48 +0000

    add style changes

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -43,6 +43,17 @@ language = "en"
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
+html_theme_options = {'logo_only' : True,
+                      'display_version' : False}
+
+html_css_files = [
+    'custom.css',
+]
+
+html_logo = "_static/phs-logo.png"
+
+html_favicon = "_static/favicon_phs.ico"
+
 # -- Options for autodoc ----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#configuration
 
