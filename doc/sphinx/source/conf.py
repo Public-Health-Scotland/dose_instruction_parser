1572bca6e2c85619fb2b807af86d3c5ac4ba73ef
Rosalyn Pearson <rosalyn.pearson@phs.scot>
2024-04-17 06:38:40 +0000

    compilation bugs

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -5,8 +5,7 @@
 
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath('../../dose_instruction_parser/di_parser'))
+sys.path.insert(0, os.path.abspath('../../../'))#os.path.abspath('../../../dose_instruction_parser/di_parser'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
